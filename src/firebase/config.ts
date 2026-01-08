af095ec00db90242054a01c49b6f266cf7dab6e8
Servant Jiki <jikiservant@gmail.com>
2026-01-08 13:33:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3001570066-83f64",
+  "appId": "1:1082438501802:web:92caa8a317f4d9d67617d3",
+  "apiKey": "AIzaSyDhC5OCgpfUWWf1aZOceWjaLSmbbD8d9vI",
+  "authDomain": "studio-3001570066-83f64.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1082438501802"
+};
