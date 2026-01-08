293cd11c66d0004ca1b63f669e0d9b9dc73814d1
Servant Jiki <jikiservant@gmail.com>
2026-01-08 13:46:55 +0000

    Try fixing this error: `Runtime ReferenceError: DialogDescription is not

--- src/components/clients/ClientsPage.tsx
+++ src/components/clients/ClientsPage.tsx
@@ -12,7 +12,7 @@ import { Client, Task } from '@/lib/types';
 import { useCollection, useFirebase, useMemoFirebase } from '@/firebase';
 import { collection, doc, Timestamp } from 'firebase/firestore';
 import { addDocumentNonBlocking, updateDocumentNonBlocking } from '@/firebase/non-blocking-updates';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogClose } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogClose, DialogDescription } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useToast } from '@/hooks/use-toast';
